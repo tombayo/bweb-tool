8fce4327f795d8eba70c5884727c1041f7cbfe6b
tombayo <me@tombayo.com>
2017-11-07 00:24:26 +0100

    UI-improvements and file cleanup.

--- bweb-tool.js
+++ bweb-tool.js
@@ -2,6 +2,46 @@
  * Main Script of the extension
  */
 
+
+/**
+ * Applies various style and DOM improvements
+ */
+function uiBooster() {
+  $('#content > h2').hide();
+  $('#content > form').hide();
+  $('#header span').first().hide();
+  $('<div style="float:left"/>').append(
+    $('<h2/>').css({marginBottom:'0px',marginTop:'1.5rem'}).append('<a href="/">Bestillingsweb</a>')
+  ).insertAfter('#header > div:first');
+  $('#header > div:last > b:last').hide().next().hide().next().hide();
+  $('#header > div:last > b:last')[0].nextSibling.remove();
+  $('#content > span.secret').hide();
+
+  // Below code changes the links to apply filters instead of loading new pages/tables (this is faster for both client and server)
+  $('#navigation > div:first a').each(function() {
+    var filter = $(this).attr('href').replace(/[^\w\s]/gi, '');
+    $(this).attr('href','#').on('click', function(){
+      filterStatus(filter);
+    });
+  });
+}
+
+/**
+ * Runs when DataTable is fully loaded, hence used to do stuff with DataTable
+ */
+function datatableLoaded() {
+  $('.chosen-select').chosen({width: "-webkit-fill-available"}); // Applies Chosen to the select-fields.
+}
+
+/**
+ * Applies a filter to the status column
+ * 
+ * @param {*String} status 
+ */
+function filterStatus(status) {
+  $('.chosen-select:last').val([status]).trigger('change').trigger('chosen:updated'); // Run the filter-change.
+}
+
 $(function(){
   var num_orders = $('#id_antall').val();
   if (num_orders != 'Alle') { // Check if number of orders is set to ALL
@@ -12,7 +52,7 @@ $(function(){
 
   // Duplicate table to remove tablesorter, and add tfoot for filtering
   var newtable = $('<table id="bweb" width="100%" class="compact row-border">')
-    .html($('#oversikt').html());
+    .html($('#oversikt').html().toLowerCase());
   var footer = $('<tfoot/>')
     .html($(newtable).find('thead').html())
     .css('display','table-header-group')
@@ -24,11 +64,12 @@ $(function(){
 
   $('#oversikt').remove(); // Remove original table (has tablesorter active on it)
 
-  // Style fix
-  //$('#bweb .header').css({paddingLeft:'4px',cursor:'default'});
+  // Style and DOM mods
+  uiBooster();
 
   $('#bweb').DataTable( {
-    stateSave: true,
+    "stateSave": true,
+    "language": {"url":"//cdn.datatables.net/plug-ins/1.10.16/i18n/Norwegian-Bokmal.json"},
     "order": [[ 0, "desc" ]], // Selects the initial ordering of the table
     "paging": false, // Defines if paging should be enabled
     "columnDefs": [ // Column number 6 is set to be invisible
@@ -38,7 +79,7 @@ $(function(){
       }
     ],
     initComplete: function () {
-      this.api().columns([3,4,5,7,8,9,10]).every( function () {
+      this.api().columns([1,3,4,5,7,8,9,10]).every( function () {
         var column = this;
         var select = $('<select data-placeholder="Filter.." class="chosen-select" multiple><option value="">Alle</option></select>')
           .appendTo( $(column.footer()).empty() )
@@ -57,7 +98,8 @@ $(function(){
             select.append( '<option value="'+d+'">'+d+'</option>' )
           }
         });
-        $('.chosen-select').chosen({width: "-webkit-fill-available"});
+
+        datatableLoaded(); // Everything is now loaded, lets run this to add more functionality.
       });
     }
   });
