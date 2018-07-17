a553d1087d3b5b275935bcdca2d002d70013f57a
Tom Andre Munkhaug <me@tombayo.com>
2018-07-17 09:00:28 +0200

    Version bump + added some functionality on comment-page

---
+++ bweb-tool.workorder.comment.js
@@ -0,0 +1,28 @@
+/**
+ * This file executes when the comment section on a workorder-page is opened.
+ */
+
+
+/**
+ * Adds the current logged in company name to a div with the id entrep.
+ * This is to re-enable some broken funtionality on the vanilla page.
+ */
+function addentrepID() {
+  var entrep = $('#header > div:last-child').html().match(/\(([^\)]+)\)/)[1]
+  $('<div id="entrep">'+entrep+'</div>').appendTo('body').hide();
+}
+
+/**
+ * Gets the contact's phonenumber of the current workorder.
+ */
+function getTlf() {
+  return $('td:contains(Telefon kontaktperson)').next().html()
+}
+
+
+$(function(){
+  addentrepID()
+  $("input[name='sms']").click(()=> {
+    $("#id_telefon").val(getTlf())
+  })
+})
